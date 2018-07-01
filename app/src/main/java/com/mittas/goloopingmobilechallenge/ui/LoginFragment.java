4b8350ea774e99fbe7ba2813b21e8a0e68c48c62
Yannis <mittas.ioannis@gmail.com>
2018-07-01 13:20:52 +0300

    edited loginfragment

--- app/src/main/java/com/mittas/goloopingmobilechallenge/ui/LoginFragment.java
+++ app/src/main/java/com/mittas/goloopingmobilechallenge/ui/LoginFragment.java
@@ -6,25 +6,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.mittas.goloopingmobilechallenge.R;
+import com.mittas.goloopingmobilechallenge.viewmodel.LoginViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class LoginFragment extends Fragment {
     public static final String TAG = "LOGIN_FRAG";
 
+    private LoginViewModel viewModel;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+
+        usernameEditText = rootView.findViewById(R.id.text_input_username);
+        passwordEditText = rootView.findViewById(R.id.text_input_password);
+        loginButton = rootView.findViewById(R.id.button_login);
+
+        return rootView;
+    }
 
 
-
-
-
-        return inflater.inflate(R.layout.fragment_login, container, false);
+    private boolean isEditTextEmpty(EditText editText) {
+        if (editText.getText().toString().trim().length() > 0) {
+            return false;
+        }
+        return true;
     }
 
 }
